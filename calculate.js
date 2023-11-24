2ee8fd4655c3c9db917374cf747ba0df20d6a451
Ramanand <118842145+Ramanandgithub@users.noreply.github.com>
2023-11-24 18:56:54 +0530

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,20 @@
+let outputscreen = document.getElementById("output-screen");
+function display(num){
+    outputscreen.value += num;
+}
+function equal(){
+    try{
+        outputscreen.value = eval(outputscreen.value);
+
+    }
+    catch(err){
+        alert("Invalid");
+    }
+
+}
+function Clear(){
+    outputscreen.value = "";
+}
+function del(){
+    outputscreen.value = outputscreen.value.slice(0,-1);
+}
